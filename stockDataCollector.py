27d59fd5030567260e19bd9ab5681b289aad0b9a
Colten Gaines <colten.gaines98@gmail.com>
2023-08-22 09:27:07 -0600

    Predicition model WIP

--- stockDataCollector.py
+++ stockDataCollector.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import pandas as pd
 from dotenv import load_dotenv
 import yfinance as yf
-from cryptoDataCollector import writeAverages
 
 
 load_dotenv()
@@ -21,21 +20,22 @@ def cleanData(df):
     Converts number types to floats and drops unneeded columns
     """
     df = df.drop(columns=["Dividends", "Stock Splits"])
-    df = df.rename(
-        columns={
-            "Date": "date",
-            "Open": "open",
-            "High": "high",
-            "Low": "low",
-            "Close": "close",
-            "Volume": "volume",
-        }
-    )
-    df["open"] = df["open"].astype(float)
-    df["high"] = df["high"].astype(float)
-    df["low"] = df["low"].astype(float)
-    df["close"] = df["close"].astype(float)
-    df["volume"] = df["volume"].astype(float)
+    # df = df.rename(
+    #     columns={
+    #         "Date": "date",
+    #         "Open": "open",
+    #         "High": "high",
+    #         "Low": "low",
+    #         "Close": "close",
+    #         "Volume": "volume",
+    #     }
+    # )
+    # df["open"] = df["open"].astype(float)
+    # df["high"] = df["high"].astype(float)
+    # df["low"] = df["low"].astype(float)
+    # df["close"] = df["close"].astype(float)
+    # df["volume"] = df["volume"].astype(float)
+    print(df.head())
     return df
 
 
@@ -46,14 +46,13 @@ def writeToCSV(df, symbol):
     path = (
         "./data/yfinance/" + symbol + "-" + datetime.now().strftime("%Y-%m-%d") + ".csv"
     )
-    df.to_csv(path, index=False)
+    df.to_csv(path, sep=',', encoding='utf-8')
 
 
 if __name__ == "__main__":
     stockList = ["MSFT", "AAPL", "TSLA"]
     for s in stockList:
         print(f"\n\n---------------------Processing:{s}---------------------")
-        df = getStockHistory(stock=s)
+        df = getStockHistory(stock=s, timespan="1y")
         df = cleanData(df)
-        writeAverages(df)
         writeToCSV(df, s)
